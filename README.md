5739c06e585e6daa322de315bdc34639b3c03f0c
pondels <yeckron@comcast.net>
2023-06-29 22:07:02 -0600

    Small Updates

--- README.md
+++ README.md
@@ -1,32 +1,76 @@
-# Creating a Docker account
-Go to the following website and create an account with github if you haven't already.
+# First Things First
+## Open up Docker Desktop
 
-```https://hub.docker.com/```
+# INSTRUCTIONS PART 1
 
-# What Now?
+## Step 1: Run this line of code
 
-### Creating a Docker Repository
-- #### Go ahead and click that fancy shmancy "Create repository" button and name it whatever you want
-![](./images/create_repo.png)
-
-- #### Once you do that, you will get a docker repository similar to the one you see above, where it's your docker username, followed by the name of the repository.
-
-### Building the docker file
-- #### Now we have to build the docker file to port it to our brand new repository.
-- #### We do this by using our DockerFile, which you can browse
-```docker build -t username/docker-repository -f ./Dockerfile .```
-
-### Pushing garbage to your streamlet-docker application
-```docker push username/docker-repository```
-```
-
-```
-
-# Grab the docker file
 ```docker pull username/repository-name```
 
-# Start the instance
+## Step 2: Run this line of code
+
 ```docker run -p 8501:8501 username/repository-name```
 
-# Launch the server under your localhost
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.   
+
+# INSTRUCTIONS PART 2
+
+## Prerequisites:
+
+ - Make sure you open this folder in VS Code and are inspecting this markdown file in a pretty format.
+
+ - Make sure yout working directory is this folder. 
+
+## Step 1: Create a Docker account
+
+Go to the following website and create an account with github if you haven't already.
+
+[Click Me to Make a Docker Account](https://hub.docker.com/)
+
+
+## Step 2: Create a Docker Repository
+
+Once you are have created an account, go to the [repositories section](https://hub.docker.com/repositories/) and click the "Create repository" button and name it whatever you want
+
+![](./images/create_repo.png)
+
+- Once you do that, you will get a docker repository similar to the one you see above, where it's your docker username, followed by the name of the repository.
+
+(For example, if your username was brighamyoung and your repository is called discourses, your repository path would be called, "*brighamyoung/discourses*")
+
+## Step 3: Building the docker file
+
+(When you are bulding your docker file, make sure you are logged in to your docker account on the docker desktop application on your computer)
+
+-  Now we have to build the docker file to port it to our brand new repository.
+-  We do this by using our Dockerfile, which you can browse
+- ##### Be sure to replace the username/docker-repository with the appropriate information 
+
+```docker build -t username/docker-repository -f ./Dockerfile .```
+
+## Step 4: Push to your streamlet-docker application
+```docker push username/docker-repository```
+
+## Step 5: Pull the docker file
+```docker pull username/repository-name```
+
+## Step 6: Start the instance
+```docker run -p 8501:8501 username/repository-name```
+
+## Step 7: Launch the server under your localhost
 http://localhost:8501/
--- app.py
+++ app.py
@@ -1,53 +1,339 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+import plotly.express as px
+import ast
+from textblob import TextBlob
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import spacy
+from spacy import displacy
 
-# Set page title
-st.set_page_config(page_title='Streamlit Demo')
 
-# Create a title and description
-st.title('Streamlit Demo App')
-st.write('This is a simple Streamlit application demonstrating various elements.')
+st.write("---")
+st.title("Section 1: Text Elements")
+st.code("""
+# streamlit text elements code
+    st.header(":red[Text Elements]")
+    st.subheader("Text Elements :sunglasses:")
+    st.write("Here are some text elements. You can use these to write text to your app.")
+    st.text("Text can also be in different formats.")
+    st.latex("e=mc^2")
+    st.code("Code can be displayed as well.")""")
+st.write("---")
 
-# Create a button
-if st.button('Click Me'):
-    st.write('Button clicked!')
+st.header(":red[Text Elements]")
+st.subheader("Text Elements :sunglasses:")
+st.write("Here are some text elements. You can use these to write text to your app.")
+st.text("Text can also be in different formats.")
+st.latex("e=mc^2")
+st.code("Code can be displayed as well.")
 
-# Create a dropdown
-option = st.selectbox('Select an option', ['Option 1', 'Option 2', 'Option 3'])
-st.write('You selected:', option)
+### PART ONE PLAYGROUND
 
-# Create a slider
-value = st.slider('Select a value', 0, 10)
-st.write('Selected value:', value)
 
-# Create a checkbox
-if st.checkbox('Check me out'):
-    st.write('Checkbox is checked!')
 
-# Create a text input
-name = st.text_input('Enter your name')
-st.write('Hello', name)
 
-# Create a dataframe
-data = {'x': np.random.randn(100), 'y': np.random.randn(100)}
-df = pd.DataFrame(data)
 
-# Create a line chart
-st.line_chart(df)
 
-# Create a bar chart
-st.bar_chart(df)
 
-# Create a scatter plot
-st.scatter_chart(df)
 
-# Create a button to download data
-if st.button('Download Data'):
-    st.download_button(
-        label='Download CSV',
-        data=df.to_csv().encode('utf-8'),
-        file_name='data.csv',
-        mime='text/csv'
+
+
+
+
+###
+
+st.write("---")
+st.title("Section 2a: Input Widgets")
+
+st.write("Checkboxes and other input widgets can be used for user input. They can be assigned to variables/represent boolean values which can be used to trigger events in your app. Below they are useless since they are not assigned to variables nor used as boolean values.")
+
+st.code("""
+# streamlit widgets code
+    st.checkbox("I am a checkbox")
+""")
+st.write("---")
+st.checkbox("I am a checkbox")
+st.write("---")
+
+st.title("Section 2b: Input Widgets as conditional triggers")
+st.write("Checkboxes and other input widgets can be used for user input. They can be assigned to variables/represent boolean values which can be used to trigger events in your app.")
+
+st.code("""
+# streamlit conditional widgit code
+    if st.checkbox("Checkbox as a conditional trigger"):
+        st.write("Checkbox was clicked")
+""")
+st.write("---")
+if st.checkbox("Checkbox as a conditional trigger"):
+    st.write("Checkbox was clicked")
+st.write("---")
+
+st.title("Section 2c: Input Widgets as variables")
+st.write("Checkboxes and other input widgets can be used to store variables.")
+
+st.code("""
+# streamlit variable/functional widgit code
+    your_input = st.text_input("This data will be stored in a variable")
+    st.write("The data you entered is: ", your_input)
+""")
+st.write("---")
+your_input = st.text_input("This data will be stored in a variable")
+st.write("The data you entered is: ", your_input)
+st.write("---")
+
+
+st.title("Section 2c: Input Widgets as variables")
+st.write("Checkboxes and other input widgets can be used to store variables (or run functions).")
+
+st.code("""
+# streamlit widgets code
+
+    if st.button("Click for a surprise"):
+        st.balloons()
+
+    st.write(":red[**Text input**]")
+    text = st.text_input('Enter some text')
+    st.write('You entered: ', text)
+
+    options = ['A', 'B', 'C', 'D', 'E', 'F']
+
+    st.write(":red[**Single radio select input**]")
+    radio = st.radio('Choose an option', options)
+    st.write('You selected: ', radio)
+
+    st.write(":red[**Multi select input**]")
+    multiselect = st.multiselect('Select multiple options', options)
+    st.write('You selected: ', multiselect)
+""")
+st.write("---")
+
+st.write(":red[**Text input**]")
+text = st.text_input('Enter some text')
+st.write('You entered: ', text)
+
+options = ['A', 'B', 'C', 'D', 'E', 'F']
+
+st.write(":red[**Single radio select input**]")
+radio = st.radio('Choose an option', options)
+st.write('You selected: ', radio)
+
+st.write(":red[**Multi select input**]")
+multiselect = st.multiselect('Select multiple options', options)
+st.write('You selected: ', multiselect)
+st.write("---")
+
+
+### PART TWO PLAYGROUND
+
+
+
+
+
+###
+
+st.title("Section 3: Visualizing Data and Dataframes")
+st.code("""
+    # streamlit data and data viz code
+    # Load in and clean data
+    pokemon_df = pd.read_csv('https://raw.githubusercontent.com/ajaverett/data/main/poke.csv')
+    pokemon_df['types'] = pokemon_df['types'].apply(ast.literal_eval)
+    all_types = list(set([x for t in pokemon_df['types'] for x in t]))
+    st.title('Pokémon Data Explorer')
+
+    # User chooses Pokémon type
+    selected_type = st.multiselect('Select a Pokémon Type', all_types)
+    filtered_df = pokemon_df[pokemon_df['types'].apply(lambda x: all(t in x for t in selected_type))]
+
+    # Display Pokémon stats
+    st.dataframe(filtered_df[['name','types','attack','defense','height_m','weight_kg']])
+
+    # Attack vs. Defense Scatter Plot
+    st.header('Attack vs. Defense Scatter Plot')
+    fig1 = (px.scatter(filtered_df, 
+                    x="attack", 
+                    y='defense', 
+                    opacity=0.8, 
+                    color='speed',
+                    hover_data=['name'], 
+                    trendline='ols')
+            .update_traces(marker_size=12, 
+                        marker_line_width=2, 
+                        marker_line_color='black'))
+
+    st.plotly_chart(fig1, use_container_width=True)
+
+    # Height vs. Weight Scatter Plot
+    st.header('Height vs. Weight Scatter Plot')
+
+    fig2 = (px.scatter(filtered_df, 
+                    x="height_m", 
+                    y='weight_kg',
+                    opacity=0.8, 
+                    color='hp',
+                    hover_data=['name'], 
+                    log_x=True, 
+                    log_y=True, 
+                    trendline="ols", 
+                    trendline_options=dict(log_x=True, log_y=True))
+            .update_traces(marker_size=12, 
+                        marker_line_width=2, 
+                        marker_line_color='black')
     )
+
+    st.plotly_chart(fig2, use_container_width=True)
+""")
+st.write("---")
+
+# Load in and clean data
+pokemon_df = pd.read_csv('https://raw.githubusercontent.com/ajaverett/data/main/poke.csv')
+pokemon_df['types'] = pokemon_df['types'].apply(ast.literal_eval)
+all_types = list(set([x for t in pokemon_df['types'] for x in t]))
+st.title('Pokémon Data Explorer')
+
+# User chooses Pokémon type
+selected_type = st.multiselect('Select a Pokémon Type', all_types)
+filtered_df = pokemon_df[pokemon_df['types'].apply(lambda x: all(t in x for t in selected_type))]
+
+# Display Pokémon stats
+st.dataframe(filtered_df[['name','types','attack','defense','height_m','weight_kg']])
+
+# Attack vs. Defense Scatter Plot
+st.header('Attack vs. Defense Scatter Plot')
+fig1 = (px.scatter(filtered_df, 
+                   x="attack", 
+                   y='defense', 
+                   opacity=0.8, 
+                   color='speed',
+                   hover_data=['name'], 
+                   trendline='ols')
+        .update_traces(marker_size=12, 
+                       marker_line_width=2, 
+                       marker_line_color='black'))
+
+st.plotly_chart(fig1, use_container_width=True)
+
+# Height vs. Weight Scatter Plot
+st.header('Height vs. Weight Scatter Plot')
+
+fig2 = (px.scatter(filtered_df, 
+                   x="height_m", 
+                   y='weight_kg',
+                   opacity=0.8, 
+                   color='hp',
+                   hover_data=['name'], 
+                   log_x=True, 
+                   log_y=True, 
+                   trendline="ols", 
+                   trendline_options=dict(log_x=True, log_y=True))
+        .update_traces(marker_size=12, 
+                       marker_line_width=2, 
+                       marker_line_color='black')
+)
+
+st.plotly_chart(fig2, use_container_width=True)
+
+
+### PART THREE PLAYGROUND
+
+
+
+
+###
+
+st.write("---")
+st.title("Section 4: Others")
+st.code("""
+# streamlit fun elements code
+    # Sentiment Analysis
+    st.subheader(':red[Example of some quick NLP]')
+    user_text = st.text_area("Enter Semtiment Text Here")
+
+    blob = TextBlob(user_text)
+    result = blob.sentiment
+
+    st.write("Sentiment polarity: ", round(result.polarity, 3))
+    st.write("Sentiment subjectivity: ", round(result.subjectivity, 3))
+
+    # Phase similiarity
+    st.subheader(':red[Example of two columns and using other libraries]')
+
+    a1, a2 = st.columns(2)
+
+    with a1:
+        user_text1 = st.text_area("Cosine Similarity between two texts")
+
+    with a2:
+        user_text2 = st.text_area("")
+
+
+    if user_text1 != "" and user_text2 != "":
+        try:
+            vectorizer = TfidfVectorizer().fit_transform([user_text1, user_text2])
+            st.write("Cosine similarity: ", round(cosine_similarity(vectorizer)[0,1], 2))
+        except Exception as e:
+            st.write("Error: ", str(e))
+    else:
+        st.write("Enter two texts to compare")
+    """)
+
+st.write("---")
+
+# Sentiment Analysis
+st.subheader(':red[Example of some quick NLP]')
+user_text = st.text_area("Enter Semtiment Text Here")
+
+blob = TextBlob(user_text)
+result = blob.sentiment
+
+st.write("Sentiment polarity: ", round(result.polarity, 3))
+st.write("Sentiment subjectivity: ", round(result.subjectivity, 3))
+
+
+# Phase similiarity
+st.subheader(':red[Example of two columns and using other libraries]')
+
+a1, a2 = st.columns(2)
+
+with a1:
+    user_text1 = st.text_area("Cosine Similarity between two texts")
+
+with a2:
+    user_text2 = st.text_area("")
+
+
+if user_text1 != "" and user_text2 != "":
+    try:
+        vectorizer = TfidfVectorizer().fit_transform([user_text1, user_text2])
+        st.write("Cosine similarity: ", round(cosine_similarity(vectorizer)[0,1], 2))
+    except Exception as e:
+        st.write("Error: ", str(e))
+else:
+    st.write("Enter two texts to compare")
+    
+
+
+### PART FOUR PLAYGROUND
+
+
+###
+
+st.write("\n\n\n")
+st.write("\n\n\n")
+st.write("\n\n\n")
+st.write("\n\n\n")
+st.write("\n\n\n")
+st.write("\n\n\n")
+st.write("\n\n\n")
+st.write("\n\n\n")
+st.write("\n\n\n")
+st.write("\n\n\n")
+st.write("\n\n\n")
+st.write("\n\n\n")
+st.write("\n\n\n")
+st.write("\n\n\n")
+st.write(":red[**Yay!**]")
+if st.button("Click for a surprise"):
+    st.balloons()
+    st.write("You did it!")
+    
